bbe6717798714b91b838415877d00ffd6f1226f5
LucasNF22 <lucasnfiorentino@gmail.com>
2023-11-08 16:27:01 -0300

    Completado controller y ruta para actualizar estado de notificacion

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,7 +2,9 @@ import NotificationModel from '../models/notificationModel';
 import { NextFunction, Request, Response } from 'express';
 import { CatchAsyncError } from '../middlewares/catchAsyncErrors';
 import ErrorHandler from '../utils/ErrorHandler';
+import { NotBeforeError } from 'jsonwebtoken';
 
+// Obtener notificaciones
 export const getNotifications = CatchAsyncError( async( req: Request, res: Response, next: NextFunction) => {
     try {
         const notifications = await NotificationModel.find().sort({ createdAt: -1 });
@@ -16,3 +18,28 @@ export const getNotifications = CatchAsyncError( async( req: Request, res: Respo
         return next(new ErrorHandler( error.message, 500));
     }
 });
+
+// Update de notificacion -- Solo admin
+export const updateNotification = CatchAsyncError( async( req: Request, res: Response, next: NextFunction) => {
+    try {
+        const notification = await NotificationModel.findById( req.params.id );
+
+        if( !notification){
+            return next( new ErrorHandler( "No se encontró la notificación", 404));
+        } else{
+            notification.status ? notification.status = 'read' : notification.status;
+        };
+
+        await notification.save();
+
+        const notificacion = await NotificationModel.find().sort({ createdAt: -1 });
+
+        res.status(201).json({
+            success: true,
+            notificacion,
+        });
+
+    } catch (error:any) {
+        return next( new ErrorHandler( error.message, 500))
+    }
+})
--- server/routes/notification.routes.ts
+++ server/routes/notification.routes.ts
@@ -1,10 +1,13 @@
 const express = require('express');
-import { getNotifications } from '../controllers/notification.controller';
+import { getNotifications, updateNotification } from '../controllers/notification.controller';
 import { authorizedRoles, isAuthenticated } from '../middlewares/auth';
 const notificationRouter = express.Router();
 
 // Obtener notificaciones
 notificationRouter.get( "/get-notifications", [isAuthenticated, authorizedRoles("admin")], getNotifications );
+
+// Update notificaciones - ADMIN
+notificationRouter.put( "/update-notification/:id", [isAuthenticated, authorizedRoles("admin")], updateNotification );
 
 
 export default notificationRouter;
