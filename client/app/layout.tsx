a111d00aa9d1d590798494e586e85ba85efb0c3a
LucasNF22 <lucasnfiorentino@gmail.com>
2023-11-20 15:47:24 -0300

    Continuando con header y page

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} ${josefine.variable}`}>
+      <body className={`${poppins.variable} ${josefine.variable} !bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}>
         <ThemeProvider attribute='"class' defaultTheme="system" enableSystem >
           { children }  
         </ThemeProvider>  
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,16 +1,26 @@
 "use client";
 import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
+import Header from './components/Header';
 
 interface Props {}
 
 const Page: FC<Props> = (props) => {
+  
+  const [open, setOpen] = useState(false);
+  const [activeIten, setActiveIten] = useState(0);
+  
   return ( 
     <div>
       <Heading 
         title="LMS APP"
         description="LMS APP es una plataforma para personas que quieran expandir sus conocimientos"
         keywords="Programacion, MERN, Redux, Machine Learning, NextJS, NodeJS, React"
+      />
+      <Header 
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeIten}
       />
     </div>
   )
