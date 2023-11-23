1f2569944cdfc930786d463f25bf307469c9c09d
LucasNF22 <lucasnfiorentino@gmail.com>
2023-11-23 13:47:21 -0300

    continuando con clases

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -10,11 +10,26 @@ type Props = {
 const Header:FC<Props> = ( props ) => {
 
   const [active, setActive] = useState(false);
-  const [openSidebar, setOpenSidebar] = useState(false)
+  const [openSidebar, setOpenSidebar] = useState(false);
+
+  if( typeof window !== "undefined" ){             // Para hacer el scrollbar sticky
+    window.addEventListener("scroll", () => {
+      if( window.scrollY > 80 ){
+        setActive(true)
+      } else{
+        setActive(false)
+      }
+    });
+  };
 
   return (
     <div className='w-full relative' >
-      <div className={`${active ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border[#ffffff1c] shadow " : '' }`} ></div>
+      <div className={`${
+        active 
+          ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border[#ffffff1c] shadow-xl transition duration-500 " 
+          : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow" 
+        }`} >
+      </div>
     </div>
   )
 };
