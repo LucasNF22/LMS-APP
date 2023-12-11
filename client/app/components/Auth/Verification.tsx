8c41aa0450281da4778e583dcfce581872fd6140
LucasNF22 <lucasnfiorentino@gmail.com>
2023-12-11 11:12:44 -0300

    Finalizacion de verificacion

--- client/app/components/Auth/Verification.tsx
+++ client/app/components/Auth/Verification.tsx
@@ -1,3 +1,4 @@
+import { styles } from '../../styles/style';
 import React, { useState, useRef, FC } from 'react';
 import toast from 'react-hot-toast';
 import { VscWorkspaceTrusted } from 'react-icons/vsc';
@@ -8,7 +9,7 @@ type Props = {
     setRoute: (route: string) => void;
 };
 
-type verifyNumber = {
+type VerifyNumber = {
   "0": string,
   "1": string,
   "2": string,
@@ -17,7 +18,7 @@ type verifyNumber = {
 
 const Verification:FC<Props> = ({setRoute}) => {
   
-  const [ invalidError, setInvalidError] = useState<boolean>(false);
+  const [ invalidError, setInvalidError ] = useState<boolean>(false);
   
   const inputRefs = [
     useRef<HTMLInputElement>(null),
@@ -26,21 +27,81 @@ const Verification:FC<Props> = ({setRoute}) => {
     useRef<HTMLInputElement>(null),
   ];
 
+  const [verifyNumber, setVerifyNumber] = useState<VerifyNumber>({
+    0: "",
+    1: "",
+    2: "",
+    3: "",
+  })
+
   const verificationHandler = async() => {
-    console.log('Verification Test');
-    
+    setInvalidError(true);
   };
 
   const HandleInputChange = ( index:number, value:string ) => {
     setInvalidError(false);
     const newVerifyNumber = {...verifyNumber, [index]: value }
+    setVerifyNumber(newVerifyNumber);
+
+    if( value === "" && index > 0 ){
+      inputRefs[ index - 1 ].current?.focus();
+    } else if( value.length === 1 && index < 3 ){
+      inputRefs[ index + 1 ].current?.focus();
+    }
   };
 
   return (
     <div>
-      Verification
+      <h1 className={`${styles.title}`}>
+        Verifica tu Cuenta
+      </h1>
+      <br />
+      <div className='w-full flex items-center justify-center mt-2'>
+        <div className='w-[80px] h-[80px] rounded-full bg-[#497df2] flex items-center justify-center'>
+          <VscWorkspaceTrusted size={40} />
+        </div>
+      </div>
+      <br />
+      <br />
+      <div className='1100px:w-[70%] m-auto flex items-center justify-around'>
+        {Object.keys(verifyNumber).map((key,index) => (
+          <input 
+            type="number"
+
+            key={key}
+            ref={inputRefs[index]}
+            className={`w-[65px] h-[65px] bg-transparent border-[3px] rounded-[10px] flex items-center text-black dark:text-white justify-center text-[18px] font-Poppins outline-none text-center 
+              ${ invalidError
+                ? "shake border-red-500"
+                : "dark:border-white border-[#0000004a]"
+              }`}
+            placeholder=''
+            maxLength={1}
+            value={verifyNumber[key as keyof VerifyNumber]}
+            onChange={(e) => HandleInputChange(index, e.target.value)}
+          />
+        ))}
+      </div>
+      <br />
+      <br />
+      <div className='w-full flex justify-center'>
+        <button
+          className={`${styles.button}`}
+          onClick={verificationHandler}
+        >
+          Verificar código
+        </button>
+      </div>
+      <br />
+      <h5 className='text-center pt-4 font-Poppins text-[14px] text-black dark:text-white'>
+              Volver a Registro? 
+              <span
+                className='text-[#2190ff] pl-1 cursor-pointer'
+                onClick={() => setRoute("Login")}
+              >Registro</span>
+      </h5>
     </div>
   )
 }
 
-export default Verification
+export default Verification;
